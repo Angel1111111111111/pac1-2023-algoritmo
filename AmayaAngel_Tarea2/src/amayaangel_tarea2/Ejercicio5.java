fe068b5c1ab991210054eef7f8b5157024e0cd11
Angel1111111111111 <angelnataren16@gmail.com>
2023-02-01 23:16:40 -0600

    Ejercicio4resuelto

---
+++ AmayaAngel_Tarea2/src/amayaangel_tarea2/Ejercicio5.java
@@ -0,0 +1,40 @@
+package amayaangel_tarea2;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Ejercicio5 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    Random rand = new Random();
+    boolean jugarDeNuevo = true;
+
+    while (jugarDeNuevo) {
+      int numeroAleatorio = rand.nextInt(20) + 1;
+      int intentos = 0;
+      int adivinanza;
+
+      System.out.println("Adivina un numero entre 1 y 20");
+      while (intentos < 3) {
+        System.out.print("Ingresa el numero a adivinar: ");
+        adivinanza = sc.nextInt();
+        if (adivinanza == numeroAleatorio) {
+          System.out.println("Correcto! Adivinaste el numero en " + (intentos + 1) + " intentos.");
+          break;
+        } else if (adivinanza < numeroAleatorio) {
+          System.out.println("El numero es mas alto.");
+        } else {
+          System.out.println("El numero es mas bajo.");
+        }
+        intentos++;
+      }
+      if (intentos == 3) {
+        System.out.println("Lo siento, has agotado tus tres intentos. El numero era " + numeroAleatorio + ".");
+      }
+      System.out.print("Quieres jugar de nuevo? (s/n): ");
+      String respuesta = sc.next();
+      jugarDeNuevo = respuesta.equals("s");
+    }
+    sc.close();
+  }
+}
