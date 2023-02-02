dd39bf6cbd247be51491327ed8e3f9d951a32380
Angel1111111111111 <angelnataren16@gmail.com>
2023-02-01 23:27:56 -0600

    NuevoProyectos

---
+++ basico/src/estructuraControl/Promedio.java
@@ -0,0 +1,30 @@
+package estructurasControl;
+
+import java.util.Scanner;
+public class Promedio {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese nota numero 1: ");
+        int num1 = sc.nextInt();
+
+        System.out.println("Ingrese nota numero 2: ");
+        int num2 = sc.nextInt();
+        
+        System.out.println("Ingrese nota numero 1: ");
+        int num3 = sc.nextInt();
+        
+        int promedio = (num1 + num2 + num3)/3;
+        System.out.println("Su promedio es de: " + promedio);
+        
+        if (promedio < 70) {
+            System.out.println("Usted reprobo"); 
+        }
+        if (promedio >= 70 && promedio < 90){
+            System.out.println("Usted aprobo, Bueno");
+        }
+        if(promedio >= 90){
+            System.out.println("Usted aprobo, Sobresaliente");
+        }
+    }
+    
+}
---
+++ basico/src/tipos/TiposaDatos.java
@@ -0,0 +1,15 @@
+package tipos;
+
+import java.util.Scanner;
+
+public class TiposaDatos{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Ingrese su edad: ");
+        int edad = sc.nextInt();
+
+        System.out.println("Su edad es: " + edad);
+    }
+}
+
